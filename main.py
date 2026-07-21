a33a3baaca5c28e3f5204e7f214b096d573fb44d
Daniel Kim <danielhkim0303@gmail.com>
2026-07-20 21:29:11 -0400

    add brute force gaemi tests & algo

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from sakak-gaemi!")
+def gaemi(n: int, L: int = 1) -> int:
 
+    if n < 1:
+        return -1
 
-if __name__ == "__main__":
-    main()
+    current_str = str(L)
+
+    for _ in range(1, n):
+        next_str = ""
+        count = 0
+        tracking_c = ""
+
+        for c in current_str:
+            if tracking_c == "":
+                tracking_c = c
+            elif tracking_c != c:
+                next_str += str(count) + tracking_c
+                tracking_c = c
+                count = 0
+            count += 1
+        
+        next_str += str(count) + tracking_c
+        current_str = next_str
+
+    return int(next_str)
